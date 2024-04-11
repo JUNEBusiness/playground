902475ec731c572017e53300b6692d485d7a70cd
JUNEBusiness <justiceomonigho@gmail.com>
2024-04-11 14:14:13 +0100

    Training examples for different concepts in CS50P.

---
+++ examples/indoor.py
@@ -0,0 +1,17 @@
+# prompts the user to respond to the question 'how are you?' and stores the response in the variable 'assignment'
+first_name =input('First name? ')
+last_name =input('Last name? ')
+
+print(f"Hello {first_name} {last_name}")
+
+for i in range(200):
+    print("|")
+    print("/")
+    print("-")
+    print("\\")
+    print("|")
+
+
+print(f"Hello {first_name} {last_name}")
+# prints out anything within its bracket
+print("Welcome!!!")
