bc0c73b70b59b7aa1fe01056281a7a2f9f331e51
JUNEBusiness <justiceomonigho@gmail.com>
2024-09-05 13:33:54 +0100

    Additional commit

---
+++ examples/examples/examples/example.py
@@ -0,0 +1,232 @@
+# Functions are blackboxes that perform certain tasks and give a certain output
+
+# name
+# action/block of caode that should perform a task
+# and a return value
+
+# function name
+def count_numbers():
+    # starts (initialized) count at zero
+    count = 0
+
+    # loop that increases each count
+    while ( count <= 100):
+        # adds 1 to count/increases count by 1
+        count += 1
+
+        # sets a condition to break the loop
+        # if count == 30:
+        #     break
+        # prints out the current count at the current stage
+        print(count)
+    # return the new count value after the loop stops coutind
+    return count
+
+
+# assigns the 'count_numbers' funtion return value to a variable called current count
+current_count = count_numbers()
+
+# prints out the current count
+print(current_count)
+
+
+# floats 0.02, 0.05353,
+# integer 4, 4, 7 ,24424
+# boolean True, False
+# null /0
+# string names, alphabets
+
+
+
+
+
+
+
+# First receive an input
+answer = input().lower()
+'''First receive an input
+    if answer is equal to fourty-two
+    or if answer is equal to fourty two
+    or if anwer is eqaul to 42 (the integer)
+    they must be case insentitive
+    make sure that the answer is correct, otherwise output no
+        if the answer is correct print yes
+        if the answer is wrong print no
+        exit code
+    '''
+
+if answer == "forty two" or answer == "forty-two" or answer == 42:
+# Functions are blackboxes that perform certain tasks and give a certain output
+
+# name
+# action/block of caode that should perform a task
+# and a return value
+
+# function name
+def count_numbers():
+    # starts (initialized) count at zero
+    count = 0
+
+    # loop that increases each count
+    while ( count <= 100):
+        # adds 1 to count/increases count by 1
+        count += 1
+
+        # sets a condition to break the loop
+        # if count == 30:
+        #     break
+        # prints out the current count at the current stage
+        print(count)
+    # return the new count value after the loop stops coutind
+    return count
+
+
+# assigns the 'count_numbers' funtion return value to a variable called current count
+current_count = count_numbers()
+
+# prints out the current count
+print(current_count)
+
+
+# floats 0.02, 0.05353,
+# integer 4, 4, 7 ,24424
+# boolean True, False
+# null /0
+# string names, alphabets
+
+
+
+
+
+
+
+# First receive an input
+answer = input().lower()
+'''First receive an input
+    if answer is equal to fourty-two
+    or if answer is equal to fourty two
+    or if anwer is eqaul to 42 (the integer)
+    they must be case insentitive
+    make sure that the answer is correct, otherwise output no
+        if the answer is correct print yes
+        if the answer is wrong print no
+        exit code
+    '''
+
+if answer == "forty two" or answer == "forty-two" or answer == 42:
+# Functions are blackboxes that perform certain tasks and give a certain output
+
+# name
+# action/block of caode that should perform a task
+# and a return value
+
+# function name
+def count_numbers():
+    # starts (initialized) count at zero
+    count = 0
+
+    # loop that increases each count
+    while ( count <= 100):
+        # adds 1 to count/increases count by 1
+        count += 1
+
+        # sets a condition to break the loop
+        # if count == 30:
+        #     break
+        # prints out the current count at the current stage
+        print(count)
+    # return the new count value after the loop stops coutind
+    return count
+
+
+# assigns the 'count_numbers' funtion return value to a variable called current count
+current_count = count_numbers()
+
+# prints out the current count
+print(current_count)
+
+
+# floats 0.02, 0.05353,
+# integer 4, 4, 7 ,24424
+# boolean True, False
+# null /0
+# string names, alphabets
+
+
+
+
+
+
+
+# First receive an input
+answer = input().lower()
+'''First receive an input
+    if answer is equal to fourty-two
+    or if answer is equal to fourty two
+    or if anwer is eqaul to 42 (the integer)
+    they must be case insentitive
+    make sure that the answer is correct, otherwise output no
+        if the answer is correct print yes
+        if the answer is wrong print no
+        exit code
+    '''
+
+if answer == "forty two" or answer == "forty-two" or answer == 42:
+# Functions are blackboxes that perform certain tasks and give a certain output
+
+# name
+# action/block of caode that should perform a task
+# and a return value
+
+# function name
+def count_numbers():
+    # starts (initialized) count at zero
+    count = 0
+
+    # loop that increases each count
+    while ( count <= 100):
+        # adds 1 to count/increases count by 1
+        count += 1
+
+        # sets a condition to break the loop
+        # if count == 30:
+        #     break
+        # prints out the current count at the current stage
+        print(count)
+    # return the new count value after the loop stops coutind
+    return count
+
+
+# assigns the 'count_numbers' funtion return value to a variable called current count
+current_count = count_numbers()
+
+# prints out the current count
+print(current_count)
+
+
+# floats 0.02, 0.05353,
+# integer 4, 4, 7 ,24424
+# boolean True, False
+# null /0
+# string names, alphabets
+
+
+
+
+
+
+
+# First receive an input
+answer = input().lower()
+'''First receive an input
+    if answer is equal to fourty-two
+    or if answer is equal to fourty two
+    or if anwer is eqaul to 42 (the integer)
+    they must be case insentitive
+    make sure that the answer is correct, otherwise output no
+        if the answer is correct print yes
+        if the answer is wrong print no
+        exit code
+    '''
+
+# if answer == "forty two" or answer == "forty-two" or answer == 42:
