a8d04a32127154f48f4cb590cd676966d561a850
JUNEBusiness <justiceomonigho@gmail.com>
2024-09-09 19:01:33 +0100

    Added more definition to what loops are and one example.

--- loops.py
+++ loops.py
@@ -1,8 +1,34 @@
-count = 10
+# count = 10
 
-while(count > 0):
-    print("count is equal to", count)
-    count = count - 1
+# while(count > 0):
+#     print("count is equal to", count)
+#     count = count - 1
+
+
+""" Imagine you are a cyber security specialist and your job is to make sure that all systems in the
+company you work for is searched and cleaned for threats of hacking or viruses during a particular
+timeframe(hourly, 10-minutes, daily, 5-seconds).
+You have a function that performs that task and have kept it safe on the systems.
+Would it not be tedious to wake up every morning, or check hourly, or every five seconds to run that
+or call that function. Now imagine you have to do it for different departments,
+or worse-since you are a remote worker- work for about 5 companies and have to run it for all their
+departments.
+
+Do no despair, we have loops in out arsenal. Loops are a paradigm that makes it possible for us
+to perform a repetitive task when a condition is met. Take for example the code below 
+    
+while True:
+    run clean()
+    wait 5 seconds
+    
+Loops have two parts:the head-which contains the conditon to check before the code runs
+-and the body-which contains the action to be performed."""
 
 
 
+# print("meow")
+# print("meow")
+# print("meow")
+
+while True:
+    print("meow")
