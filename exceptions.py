3e65e40f4d29ceecf4f58f51790be78e1ce50ea2
JUNEBusiness <justiceomonigho@gmail.com>
2024-10-05 20:21:43 +0100

    Added Examples for exceptions.

---
+++ exceptions.py
@@ -0,0 +1,38 @@
+
+# try:
+#     fruits = ["Mango","Apple","Orange", "Banana", "O"]
+#     positions = int(input("Give me the amount of positions you want to iterate over: "))
+#     for position in range(positions):
+#         print(fruits[position], fruits[position][3])
+# except (IndexError):
+#     print("Error message: You have indexed in a list and exceeded the range")
+
+
+# try:
+#     fruits = ["Mango","Apple","Orange", "Banana", "O"]
+#     positions = int(input("Give me the amount of positions you want to iterate over: "))
+#     for position in range(positions):
+#         print(fruits[position], fruits[position][3])
+# except (IndexError):
+#     print("Error message: You have indexed in a list and exceeded the range")
+# except ValueError:
+#     print("You have inputed a string where you are supposed to input an integer.")
+
+
+# try:
+#     fruits = ["Mango","Apple","Orange", "Banana", "O"]
+#     positions = int(input("Give me the amount of positions you want to iterate over: "))
+#     for position in range(positions):
+#         print(fruits[position], fruits[position][3])
+# except (IndexError, ValueError):
+#     print("Error message: You have indexed in a list and exceeded the range or You have inputed a string where you are supposed to input an integer.")
+
+
+
+# try:
+#     fruits = ["Mango","Apple","Orange", "Banana", "O"]
+#     positions = int(input("Give me the amount of positions you want to iterate over: "))
+#     for position in range(positions):
+#         print(fruits[position], fruits[position][3])
+# except:
+#     print("Error message: You have indexed in a list and exceeded the range or You have inputed a string where you are supposed to input an integer.")
