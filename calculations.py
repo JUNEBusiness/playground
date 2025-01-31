60b3ac34f50d5c3f7cfbd303ff49c51ecb42484e
JUNEBusiness <justiceomonigho@gmail.com>
2025-01-31 18:49:52 +0100

    added some lines of code to free.py and deleted some stackup files.

---
+++ calculations.py
@@ -0,0 +1,37 @@
+
+# x = int(input("Give me a number: "))
+# y = input("What operation do you want to do(-,/,%,*,+): ")
+# z = int(input("Give me another number: "))
+
+
+# if y == "-":
+#     print(x-z)
+# elif y == "/":
+#     print(x/z)
+# elif y == "*":
+#     print(x*z)
+# elif y == "+":
+#     print(x+z)
+# elif y == "%":
+#     print(x%z)
+# else:
+#     print("Please, try using the operation (-,/,%,*,+) for Z")
+
+
+# fruits = ["Cucumber", "apple", "orange", 12, 3.142]
+# print(fruits)
+# print(fruits[0])
+# print(fruits[1])
+
+# print(fruits[3])
+
+names = {"Attendant1":"Desire",
+         "Attendant2":"Sampson",
+         "Attendant3":"Farida",
+         "Attendant4":"Blessing",
+         "Attendant5":"Jacob",
+         "Total Attendance":5}
+
+print(names["Attendant1"])
+print(names["Attendant3"])
+print(names["Total Attendance"])
--- free.py
+++ free.py
@@ -1,6 +1,18 @@
-fruits =  ["mango", "banana", "apple", "watermelon", "cucumber"]
+# fruits =  ["mango", "banana", "apple", "watermelon", "cucumber"]
+# # print(len(fruits))
+# # print(range(len(fruits)))
+# for element in range(len(fruits)):
+#     print(fruits[element])
+# # [0,1,2,3,4]
 
-print(fruits)
-print(fruits[0])
-print(fruits[2])
-print(fruits[4])
+# check_split_result = input("Give us something to uce split method on: ").split(":")
+# hour = int(check_split_result[0])
+# minutes = int(check_split_result[1])
+
+# print(hour + 0.5)
+
+
+name = input("What is your name? ")
+ 
+print(f"My name is {name}")
+
